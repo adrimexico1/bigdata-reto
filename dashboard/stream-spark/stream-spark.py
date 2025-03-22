aa8976ba5f0e27506770498137b32e8775a3ada1
adrimexico1 <adriallison9@gmail.com>
2025-03-21 19:47:00 -0600

    fix dash

--- dashboard/stream-spark/stream-spark.py
+++ dashboard/stream-spark/stream-spark.py
@@ -32,7 +32,7 @@ def post_spark_job(user, repo, job, token, codeurl, dataseturl):
     # Display the response in the app
     st.write(response)
 
-st.title("Spark & streamlit")
+st.title("Spark & streamlit dashboard Drivers by. adrimexico1") 
 
 st.header("spark-submit Job")
 
@@ -54,9 +54,16 @@ def get_spark_results(url_results):
     if  (response.status_code ==  200):
         st.write(response.json())
 
-st.header("spark-submit results")
+st.header("spark-submit all results")
 
-url_results=  st.text_input('URL results', value='https://raw.githubusercontent.com/adrimexico1/spark-labs/refs/heads/main/results/data.json')
+url_results=  st.text_input('URL results', value='https://raw.githubusercontent.com/adrimexico1/bigdata-reto/refs/heads/main/results/data.json')
 
 if st.button("GET spark results"):
     get_spark_results(url_results)
+
+st.header("spark-submit summry results")
+
+url_results_s=  st.text_input('URL results', value='https://raw.githubusercontent.com/adrimexico1/bigdata-reto/refs/heads/main/results/summary.json')
+
+if st.button("GET spark results"):
+    get_spark_results(url_results_s)
